e05cda6c6ac21266f74bbff3a69a047afc7aec8b
ChristianObuya <christian.obuya@strathmore.edu>
2025-07-15 10:09:41 +0300

    Edited code

--- 03_algorithms/sorting/selection_sort.py
+++ 03_algorithms/sorting/selection_sort.py
@@ -5,18 +5,15 @@ def selection_sort(unsorted_list):
         minimum_index = outerloop
         
         for innerloop in range(outerloop + 1, number_of_items):
-            # print(f"Minimum is {unsorted_list[minimum_index]}")
             if unsorted_list[innerloop] < unsorted_list[minimum_index]:
                 minimum_index = innerloop
                 
         temp = unsorted_list[outerloop]
         unsorted_list[outerloop] = unsorted_list[minimum_index]
         unsorted_list[minimum_index] = temp
-        # print(f"Outerloop {unsorted_list}")
         
     return unsorted_list
 
-# values = [5,4,3,2,1]
 values = [15,12, 12,60,5,7]
 print(f"Unsorted List {values}")
 
